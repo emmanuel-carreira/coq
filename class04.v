837d950ff1c4fca5849f39f9893d9b374b2cbe9e
Emmanuel Carreira Alves <eca2@WINDOWS.CIN.UFPE.BR>
2019-08-20 11:45:44 -0300

    Finished class 04

--- class04.v
+++ class04.v
@@ -21,3 +21,30 @@ Proof.
   rewrite <- G.
   reflexivity.
 Qed.
+
+(* Se não for usar hipótese indutiva, não usar inductive, usar destruct *)
+
+(** **** Exercise: (zero_nbeq_plus_1)  *)
+(** Prova a seguinte afirmação usando [destruct] *)
+Theorem zero_nbeq_plus_1 : forall n : nat,
+  beq_nat 0 (n + 1) = false.
+Proof.
+  (* COMPLETE AQUI *) 
+  intros. destruct n.
+  - simpl. reflexivity.
+  - simpl. reflexivity. 
+Qed.
+
+(** **** Exercise: (andb_true_elim2)  *)
+(** Prova a seguinte afirmação usando [destruct] *)
+
+Theorem andb_true_elim2 : forall b c : bool,
+  andb b c = true -> c = true.
+Proof.
+  (* COMPLETE AQUI *) 
+  intros b c. destruct b, c.
+  - simpl. intro. reflexivity.
+  - simpl. intro. rewrite H. reflexivity.
+  - simpl. intro. inversion H.
+  - simpl. intro. rewrite H. reflexivity.
+Qed.
